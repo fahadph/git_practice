78fc59936f03ea32702583313fa6f9e639478d7a
Jitendra Gupta <er.jgupta2gmail.com>
2016-12-22 16:26:49 +0530

    todays new commit

--- test.js
+++ test.js
@@ -1,16 +1,14 @@
 //just created new file
-function Fahad(sub) {
-	console.log('fahad is teaching ' + sub);
 function func(){
-<<<<<<< HEAD
-	
-}
-console.log('changed again');
-
-Fahad('Git with GitBash');
-=======
 
 }
 erewrw
 asdasd
->>>>>>> 9c10f6f84af6b978a2aa38690f3f403d402da40e
+change by siddique
+Fahad('Git with GitBash');
+
+console.log("Done");
+console.log("hello");
+SKNDckjnk
+dsjhvhdvhshgcvhgv
+"merging example"
