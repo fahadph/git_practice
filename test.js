8ccd29f73503410f043a59d65ee4ae5b21f747da
ajaykumarsana <ajaykumar.sana@gmail.com>
2016-12-22 16:21:43 +0530

    commentat 4.21

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-
-function Fahad(sub) {
-	console.log('fahad is teaching' + sub);
+//just created new file
+function func(){
+	
+function func(){
+	Say hello world Mrudula
 }
 
+}
+erewrw
+asdasd
+change by siddique
 Fahad('Git with GitBash');
+
+console.log("Done");
+console.log("hello");
